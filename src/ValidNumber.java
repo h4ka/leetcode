4fc276590e504353f6895e4ef89005ca5f48ccf2
Andrei Zorin <andrei.zorin@artin.cz>
2019-02-03 15:02:07 +0100

    passed all tests

--- src/ValidNumber.java
+++ src/ValidNumber.java
@@ -2,9 +2,11 @@ public class ValidNumber {
 
     public boolean isNumber(String s) {
 
-        boolean result = false;
-
         s = s.trim();
+
+        if (s.startsWith("e") || s.endsWith("e")){
+            return false;
+        }
 
         String[] tokens = s.split("e");
 
@@ -16,9 +18,6 @@ public class ValidNumber {
             expString = tokens[1];
         }
         else if (tokens.length == 1){
-            if (s.startsWith("e") || s.endsWith("e")){
-                return false;
-            }
             digitString = tokens[0];
             expString = null;
         } else {
@@ -30,12 +29,107 @@ public class ValidNumber {
 
     private boolean validateExp(String expString) {
 
+        if (expString == null) return true;
 
+        char[] chars = expString.toCharArray();
+
+        int idx = 0;
+
+        for (char c : chars) {
+
+            if (idx == 0){
+                if (c == '+' || c == '-') {
+                    idx++;
+                    continue;
+                }
+            }
+
+            if (c < 48 || c > 57) {
+                return false;
+            }
+
+            idx++;
+        }
+
+        return true;
 
     }
 
     private boolean validateDigit(String digitString) {
 
+
+        char[] chars = digitString.toCharArray();
+
+        boolean plusMinus = false;
+        boolean floating = false;
+        boolean zero = false;
+
+        int idx = 0;
+
+        for (char c : chars) {
+
+            if (idx == 0){
+                if (c == '+' || c == '-') {
+                    plusMinus = true;
+                    idx++;
+                    continue;
+                }
+                else if (c == '0') {
+                    zero = true;
+                    idx++;
+                    continue;
+                } else if (c < 49 || c > 57)
+                    return false;
+
+            }
+            else if (idx == 1) {
+
+                if (plusMinus) {
+                    if (c == '0') {
+                        zero = true;
+                        idx++;
+                        continue;
+                    }
+                }
+                else if ( zero ){
+                    if (c != '.') {
+                        return false;
+                    } else {
+                        floating = true;
+                        idx++;
+                        continue;
+                    }
+                }
+
+            }
+            else if (idx == 2) {
+                if (plusMinus && zero) {
+                    if (c == '.') {
+                        floating = true;
+                        idx++;
+                        continue;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+
+            if (c < 48 || c > 57) {
+                if (c == '.'){
+                    if (floating) {
+                        return false;
+                    }
+                    else {
+                        floating = true;
+                    }
+                } else {
+                    return false;
+                }
+            }
+            idx++;
+        }
+
+        return true;
     }
 
 }
