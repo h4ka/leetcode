63bb8b72af6dd117d892a93dde0f4bb70df9a72c
Andrei Zorin <andrei.zorin@artin.cz>
2019-02-04 13:04:53 +0100

    add PalindromicSubstring problem

---
+++ test/PalindromicSubstringTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PalindromicSubstringTest {
+
+    private PalindromicSubstring palindromicSubstring = new PalindromicSubstring();
+
+    @Test
+    public void countSubstrings() {
+
+        assertEquals(3, palindromicSubstring.countSubstrings("abc"));
+        assertEquals(6, palindromicSubstring.countSubstrings("aaa"));
+
+
+    }
+}
