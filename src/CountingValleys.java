38329f6d381bab2fb0747ca34755bd38135260fe
Andrei Zorin <andrei.zorin@artin.cz>
2019-06-16 22:18:32 +0200

    add Counting Valleys task

---
+++ src/CountingValleys.java
@@ -0,0 +1,66 @@
+/***
+ * Gary is an avid hiker. He tracks his hikes meticulously, paying close attention to small details like topography.
+ * During his last hike he took exactly n steps. For every step he took, he noted if it was an uphill, U, or a downhill, D step.
+ * Gary's hikes start and end at sea level and each step up or down represents a 1 unit change in altitude.
+ * We define the following terms:
+ *
+ * A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
+ * A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
+ * Given Gary's sequence of up and down steps during his last hike, find and print the number of valleys he walked through.
+ *
+ * For example, if Gary's path is s = [DDUUUUDD], he first enters a valley 2 units deep.
+ * Then he climbs out an up onto a mountain 2 units high.
+ * Finally, he returns to sea level and ends his hike.
+ *
+ * Function Description
+ *
+ * Complete the countingValleys function. It must return an integer that denotes the number of valleys Gary traversed.
+ *
+ * countingValleys has the following parameter(s):
+ *
+ * n: the number of steps Gary takes
+ * s: a string describing his path
+ *
+ * Sample Input
+ * 8
+ * UDDDUDUU
+ *
+ * Sample Output
+ * 1
+ *
+ *
+ */
+
+
+
+public class CountingValleys {
+
+    static int countingValleys(int n, String s) {
+
+        int ref = 0;
+        int counter = 0;
+        boolean isInValley = false;
+
+        for (char c : s.toCharArray()){
+
+            if (c == 'U') ref++;
+            if (c == 'D') ref--;
+
+            if (ref < 0 ) {
+                if (!isInValley) {
+                    isInValley = true;
+                    counter++;
+                }
+            } else {
+                isInValley = false;
+            }
+
+        }
+
+        return counter;
+
+    }
+
+
+
+}
